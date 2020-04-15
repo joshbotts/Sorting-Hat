46dfd98651467112d53e6ae1cf521e1e4e4f0c15
joshbotts <jbotts@gmail.com>
2020-04-15 16:08:51 -0400

    Initial Commit

---
+++ Sorting Hat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sorting Hat
+//
+//  Created by Joshua Botts on 4/15/20.
+//  Copyright © 2020 Joshua Botts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
