35a7381243303e423db0dc2e4bd9de49c55e9974
joshbotts <jbotts@gmail.com>
2020-09-16 22:29:29 -0400

    iOS 14 update continued

--- Sorting Hat/ContentView.swift
+++ Sorting Hat/ContentView.swift
@@ -11,94 +11,19 @@ import Combine
 import AVFoundation
 
 struct ContentView: View {
-    @State var userName: String = ""
-    @ObservedObject var store = SortingStore()
-    @State var storeSet: Bool = false
-    //    @Environment(\.colorScheme) var colorScheme
-    let textFieldPlaceholderText = "Enter your name and press return."
-    @State var kidsMode: Bool = false
-    @ObservedObject private var keyboard = KeyboardResponder()
-    @State var talkingHat: Bool = false
-    
-    func speakSortingHat() {
-        let utterance = AVSpeechUtterance(string: "Oh, yes. \(self.userName). Let's sort you out.")
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        utterance.rate = 0.3
-        
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-    }
-    
-    func textSortingHat() {
-        self.userName = "Ahhhh, yes. Let's sort you out."
-    }
-    
+    @EnvironmentObject var store: SortingStore
     
     var body: some View {
-        NavigationView {
-            VStack {
-                VStack(alignment: .center) {
-                    Image("sorting-hat")
-                        .resizable()
-                        .scaledToFit()
-                }
-                VStack {
-                    TextField(self.textFieldPlaceholderText, text: self.$userName, onCommit: {
-                        self.kidsMode ? self.store.setKidStore(user: self.userName) : self.store.setStore(user: self.userName);
-                        self.storeSet = true
-                        print("User \(self.userName) is set. The Sorting Store has loaded these questions: \(self.store.questions.values.map({$0.question}))")
-                        self.talkingHat ? self.speakSortingHat() : self.textSortingHat()
-                    })
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .disableAutocorrection(true)
-                    if self.storeSet == false {
-                        Toggle(isOn: self.$kidsMode) {
-                            Text("Want to use Kid's Mode?")
-                        }
-                        .font(.body)
-                        Toggle(isOn: self.$talkingHat) {
-                            Text("Want the Hat to speak?")
-                        }
-                        .font(.body)
-                    }
-                }
-                .padding()
-                .padding(.bottom, keyboard.currentHeight)
-                .edgesIgnoringSafeArea(.bottom)
-                .animation(.easeOut(duration: 0.16))
-                //                .background(colorScheme == .dark ? Color.black : Color.white)
-                HStack {
-                    Spacer()
-                    if self.storeSet == true {
-                        Button("Reset the Sorting Hat",
-                               action: {
-                                self.userName = ""
-                                self.store.resetStore()
-                                self.storeSet = false
-                        }
-                        )
-                            .font(.body)
-                    }
-                }
-                .padding(10)
-                VStack(alignment: .center) {
-                    if self.storeSet == true {
-                        Spacer()
-                        NavigationLink(destination: SortingView(store: self.store, question: self.store.questions.keys.randomElement()!, kidsMode: self.kidsMode, talkingHat: self.talkingHat)) {
-                            Text("Begin the Sorting!")
-                                .font(.headline)
-                        }
-                        .isDetailLink(false)
-                        //                .background(colorScheme == .dark ? Color.black : Color.white)
-                    }
-                }
-                .padding()
-                .frame(maxWidth: 700, alignment: .center)
-            }
-            .padding()
+        switch self.store.mode {
+        case SortingStore.Mode.start:
+            StartingView()
+        case SortingStore.Mode.settings:
+            SettingsView(questionsToAsk: self.store.questionsForSort)
+        case SortingStore.Mode.sort:
+            SortingView()
+        case SortingStore.Mode.results:
+            SortingResultView()
         }
-        .padding()
-        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
@@ -108,29 +33,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-class KeyboardResponder: ObservableObject {
-    private var notificationCenter: NotificationCenter
-    @Published private(set) var currentHeight: CGFloat = 0
-    
-    init(center: NotificationCenter = .default) {
-        notificationCenter = center
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    deinit {
-        notificationCenter.removeObserver(self)
-    }
-    
-    @objc func keyBoardWillShow(notification: Notification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            currentHeight = keyboardSize.height
-        }
-    }
-    
-    @objc func keyBoardWillHide(notification: Notification) {
-        currentHeight = 0
-    }
-}
-
-// implemented view response to keyboard activation from https://stackoverflow.com/a/58242249/12259419
--- Sorting Hat/SortingView.swift
+++ Sorting Hat/SortingView.swift
@@ -9,228 +9,24 @@
 import SwiftUI
 import Combine
 import AVFoundation
+import AVKit
 
 struct SortingView: View {
-    @Environment(\.presentationMode) var presentationMode
-    @ObservedObject var store: SortingStore
+    @EnvironmentObject var store: SortingStore
     @State var sortingTime: Bool = false
-    @State var questionsAsked: Int = 0
-    @State var currentQuestion: Int
-    var kidsMode: Bool
-    var talkingHat: Bool
-    
-    init(store: SortingStore, question: Int, kidsMode: Bool, talkingHat: Bool) {
-        self._currentQuestion = /*State<Int>*/.init(initialValue: question)
-        self.store = store
-        self.kidsMode = kidsMode
-        self.talkingHat = talkingHat
-        print("The Sorting View has received the Sorting Store")
-        print("The current question is \(store.questions[self.currentQuestion]!.question)")
-        print(self.kidsMode ? "The Sorting Hat is in Kid's Mode" : "The Sorting Hat is not in Kid's Mode")
-        print(self.talkingHat ? "The Sorting Hat will automatically speak" : "The Sorting Hat will only speak when asked")
-    }
-    
-    // learned how to initialize a @State wrapped property at https://stackoverflow.com/questions/59920657/swiftui-documentation-for-state-property-wrapper
-    
-    func getQuestion() -> Int {
-        var unaskedQuestions = Dictionary<Int, SortingQuestion>()
-        for question in self.store.questions {
-            if question.value.asked == 0 {
-                unaskedQuestions[question.key] = question.value
-            }
-        }
-        if unaskedQuestions.isEmpty {
-            self.sortingTime = true
-            print("No more unasked questions. Time to sort.")
-        }
-        return unaskedQuestions.randomElement()!.key
-    }
-    
-    func scoreQuestion(choice: SortingQuestion.SortingQuestionChoice) {
-        self.store.questionScorer(user: self.store.user, question: self.store.questions[currentQuestion]!, choice: choice)
-    }
-    
-    func refreshView() {
-        self.questionsAsked += 1
-        var unaskedQuestions: [SortingQuestion] = []
-        for question in self.store.questions.values {
-            if question.asked == 0 {
-                unaskedQuestions.append(question)
-            }
-        }
-        if unaskedQuestions.isEmpty || self.questionsAsked > 4 {
-            self.sortingTime = true
-            print("Time to sort!")
-        } else {
-            self.currentQuestion = self.getQuestion()
-            print("The current question is \(self.store.questions[currentQuestion]?.question ?? "*none*")")
-        }
-    }
-    
-    func sortingResult() -> SortingDestination {
-        print("Sorting scores are: \(String(describing: self.store.user.sortingScores))")
-        return self.store.user.sortingScores!.max(by: { a, b in a.value < b.value })?.key ?? SortingDestination(name: "Error", descriptionFull: "The Sorting Hat needs to be fixed.")
-    }
-    
-    func speakQuestion() {
-        let utterance = AVSpeechUtterance(string: self.store.questions[currentQuestion]!.question)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        utterance.rate = 0.3
-        
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-    }
-    
-    // implemented text to speech thanks to Paul Hudson: https://www.hackingwithswift.com/example-code/media/how-to-convert-text-to-speech-using-avspeechsynthesizer-avspeechutterance-and-avspeechsynthesisvoice
-    
-    func speakChoice(choice: SortingQuestion.SortingQuestionChoice) {
-        let utterance = AVSpeechUtterance(string: choice.choiceText)
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-        utterance.rate = 0.3
-        
-        let synthesizer = AVSpeechSynthesizer()
-        synthesizer.speak(utterance)
-    }
-    
+
     var body: some View {
-        VStack(alignment: .center) {
-            if sortingTime == false {
-                if kidsMode == true {
-                    VStack {
-                        Button(action: {
-                            self.speakQuestion()
-                        }) {
-                            Text(self.store.questions[currentQuestion]!.question)
-                                .font(.headline)
-                                .lineLimit(nil)
-                                .layoutPriority(1)
-                                .fixedSize(horizontal: false, vertical: true)
-                                .padding(.horizontal, 10)
-                                .padding(.bottom, 20)
-                        }
-                        .lineLimit(nil)
-                        .fixedSize(horizontal: false, vertical: true)
-                    }
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack {
-                            ForEach(self.store.questions[currentQuestion]!.choices, id: \.self.id) { choice in
-                                Button(action: {
-                                    self.scoreQuestion(choice: choice)
-                                    self.refreshView()
-                                })  {
-                                    Image(choice.choiceImage!)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: 300, maxHeight: 300)
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                            }
-                        }
-                    }
-                    .navigationBarHidden(true)
-                    .navigationBarBackButtonHidden(true)
-                    .padding(.bottom, 20)
-                } else {
-                    VStack {
-                        VStack {
-                            Button(action: {
-                                self.speakQuestion()
-                            }) {
-                                Text(self.store.questions[currentQuestion]!.question)
-                                    .font(.headline)
-                                    .lineLimit(nil)
-                                    .layoutPriority(1)
-                                    .fixedSize(horizontal: false, vertical: true)
-                                    .padding(.horizontal, 10)
-                                    .padding(.bottom, 20)
-                            }
-                            .lineLimit(nil)
-                            .fixedSize(horizontal: false, vertical: true)
-                        }
-                        ForEach(self.store.questions[currentQuestion]!.choices, id: \.self.id) { choice in
-                            HStack {
-                                Button(choice.choiceText, action: {
-                                    self.scoreQuestion(choice: choice)
-                                    self.refreshView()
-                                })
-                                    .layoutPriority(2)
-                                    .lineLimit(nil)
-                                    .fixedSize(horizontal: false, vertical: true)
-                                Spacer()
-                                if self.talkingHat == true {
-                                    Button(action: {
-                                        self.speakChoice(choice: choice)
-                                    }) { Image(systemName: "speaker.2.fill") }
-                                }
-                            }
-                            .lineLimit(nil)
-                            .fixedSize(horizontal: false, vertical: true)
-                            .padding(.horizontal, 10)
-                            .padding(.bottom, 20)
-                        }
-                    }
-                    .navigationBarHidden(true)
-                    .navigationBarBackButtonHidden(true)
-                    .padding(.bottom, 20)
-                }
-            } else {
-                VStack(alignment: .center) {
-                    DestinationMovie(player: AVPlayer(url: Bundle.main.url(forResource: self.sortingResult().name, withExtension: "mov")!))
-                    Spacer()
-                    Text("\(self.store.user.name), you strike me as a...")
-                    Spacer()
-                    Text(self.sortingResult().name == "Dumbledore's Army" ? "member of \(self.sortingResult().name)!" : "\(self.sortingResult().name)!")
-                    Spacer()
-                    Text(self.sortingResult().descriptionFull)
-                        .font(.caption)
-                        .padding()
-                    Spacer()
-                }
-                .padding()
-                .navigationBarHidden(false)
-                .navigationBarBackButtonHidden(false)
-                .navigationBarItems(leading: Button("Sort Again?") {
-                    self.presentationMode.wrappedValue.dismiss()
-                })
-            }
+        switch self.store.kidsMode {
+        case true:
+            KidSortingView(question: self.store.questions.keys.randomElement()!)
+        case false:
+            RegularSortingView(question: self.store.questions.keys.randomElement()!)
         }
-        .frame(maxWidth: 700, alignment: .center)
     }
 }
 
-struct DestinationMovie: View, UIViewRepresentable {
-    let player: AVPlayer
-    
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<DestinationMovie>) {
-        player.play()
-    }
-    func makeUIView(context: Context) -> UIView {
-        return PlayerUIView(player: player)
+struct SortingView_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
-
-// learned how to implement AVPlayer through a SwiftUI View at https://medium.com/flawless-app-stories/avplayer-swiftui-part-2-player-controls-c28b721e7e27
-// tried to also implement a ZStack-overlaid transparent button over the DestinationMovie view to allow users to replay the sorting video, but could not figure out how to set the coordinator object in the DestinationMovie view to allow the player to receive events from the the overlaid button
-
-class PlayerUIView: UIView {
-    let playerLayer = AVPlayerLayer()
-    init(player: AVPlayer) {
-        super.init(frame: .zero)
-        
-        playerLayer.player = player
-        layer.addSublayer(playerLayer)
-    }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        playerLayer.frame = bounds
-    }
-}
-
-//struct SortingView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-//    }
-//}
